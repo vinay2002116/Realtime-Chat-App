ccbd013fa800a52a231348a5081738068bdd5f2c
vinay2002116 <77984977+vinay2002116@users.noreply.github.com>
2023-07-21 01:49:26 +0530

    Update authPage.js

--- frontend/src/authPage.js
+++ frontend/src/authPage.js
@@ -11,7 +11,7 @@ const AuthPage = (props) => {
   const onLogin = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/login", { username, secret })
+      .post("http://localhost:3000/login", { username, secret })
       .then((r) => props.onAuth({ ...r.data, secret })) // NOTE: over-ride secret
       .catch((e) => console.log(JSON.stringify(e.response.data)));
   };
@@ -19,7 +19,7 @@ const AuthPage = (props) => {
   const onSignup = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/signup", {
+      .post("http://localhost:3000/signup", {
         username,
         secret,
         email,
